da7774e8165e1b8684b9fc01b6ba31b8afc4e9dc
🎈 <numofran6@gmail.com>
2022-08-13 08:57:35 +0000

    Global error handler for the App

---
+++ errorController.js
@@ -0,0 +1,30 @@
+const devError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const prodError = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(400).send(err);
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  if (process.env.WORKENV === 'development') {
+    devError(err, res);
+  }
+  if (process.env.WORKENV === 'production') {
+    prodError(err, res);
+  }
+};
